ba05e6f46d3f83f877f9b813801247d39288a0fe
黄艺华 <lernanto@foxmail.com>
2022-02-18 11:34:35 +0800

    add comment for contrastive encoder

--- encoder.py
+++ encoder.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3 -O
 # -*- encoding: utf-8 -*-
 
+'''
+使用编码器训练方言音系 embedding.
+'''
+
+__author__ = '黄艺华 <lernanto@foxmail.com>'
+
+
 import sys
 import numpy as np
 import tensorflow as tf
@@ -10,27 +17,56 @@ import recon
 
 input_file = sys.argv[1]
 
-data = recon.clean(recon.load(input_file)
+data = recon.clean(recon.load(input_file))
 
 for c in data.columns:
     if c.partition('_')[2] in ('聲母', '韻母', '調值'):
         data[c] = data[c].astype('category')
 
 class ContrastiveEncoder:
+    '''
+    使用孪生网络 + 对比损失训练方言音系 embedding.
+
+    从包含多个方言声母、韵母、声调的同一个样本中随机抽样两份、每份包含部分方言数据的输入，
+    输入孪生网络编码成 embedding 后，采用对比损失，即相同样本距离越近越好，不同样本距离必须大于预先指定的边界。
+    '''
+
     def __init__(self, input_size, emb_size):
-        self.embedding = tf.Variable(tf.random_normal_initializer()(shape=(input_size, emb_size), dtype=tf.float32))
+        self.embedding = tf.Variable(tf.random_normal_initializer()(
+            shape=(input_size, emb_size),
+            dtype=tf.float32)
+        )
         self.trainable_variables = (self.embedding,)
 
+    @tf.function
     def encode(self, inputs):
-        weight = tf.linalg.normalize(tf.cast(inputs != -1, tf.float32), ord=1, axis=1)[0]
-        return tf.reduce_sum(tf.nn.embedding_lookup(self.embedding, tf.maximum(inputs, 0)) * tf.expand_dims(weight, -1), axis=1)
+        weight = tf.linalg.normalize(
+            tf.cast(inputs != -1, tf.float32),
+            ord=1,
+            axis=1
+        )[0]
+        return tf.reduce_sum(
+            tf.nn.embedding_lookup(self.embedding, tf.maximum(inputs, 0)) \
+                * tf.expand_dims(weight, -1),
+                axis=1
+            )
 
+    @tf.function
     def loss(self, inputs, targets):
         margin = 1
-        d2 = tf.reduce_sum(tf.square(tf.expand_dims(self.encode(inputs), 1) - tf.expand_dims(self.encode(targets), 0)), axis=1)
+        d2 = tf.reduce_sum(tf.square(
+            tf.expand_dims(self.encode(inputs), 1) \
+                - tf.expand_dims(self.encode(targets), 0)
+        ), axis=1)
         d = tf.sqrt(d2)
-        return tf.reduce_mean(tf.eye(inputs.shape[0], dtype=tf.float32) * d2 + (1 - tf.eye(inputs.shape[0], dtype=tf.float32)) * tf.square(tf.maximum(0, margin - d)), axis=1)
+        return tf.reduce_mean(
+            tf.eye(inputs.shape[0], dtype=tf.float32) * d2 \
+                + (1 - tf.eye(inputs.shape[0], dtype=tf.float32)) \
+                * tf.square(tf.maximum(0, margin - d)),
+            axis=1
+        )
 
+    @tf.function
     def update(self, inputs, targets, optimizer):
         with tf.GradientTape() as tape:
             loss = self.loss(inputs, targets)
@@ -39,6 +75,10 @@ class ContrastiveEncoder:
         return loss
 
 def gen_sample(data, sample=None):
+    '''
+    生成孪生网络的输入.
+    '''
+
     def gen():
         input_shapes = [len(d.categories) for d in data.dtypes]
         base = np.cumsum(input_shapes) - input_shapes
@@ -47,7 +87,10 @@ def gen_sample(data, sample=None):
             if sample is None:
                 yield indices
             elif len(indices) > sample:
-                yield np.random.choice(indices, sample, replace=False), np.random.choice(indices, sample, replace=False)
+                yield (
+                    np.random.choice(indices, sample, replace=False),
+                    np.random.choice(indices, sample, replace=False)
+                )
 
     return gen
 
