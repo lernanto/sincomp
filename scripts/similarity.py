6a3b86b34749eef79c0e73ce6a786a0fd353b414
黄艺华 <lernanto@foxmail.com>
2022-12-17 10:05:21 +0800

    use new library utilities

---
+++ scripts/similarity.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3 -O
+# -*- coding: utf-8 -*-
+
+"""
+计算方言之间的预测相似度.
+
+如果 A 方言的音类能很大程度预测 B 方言的音类，表明 A 方言到 B 方言单向的相似度高
+"""
+
+__author__ = '黄艺华 <lernanto@foxmail.com>'
+
+
+import argparse
+import logging
+import pandas
+
+import sinetym
+from sinetym.datasets import zhongguoyuyan
+from sinetym import similarity
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(globals().get('__doc__'))
+    parser.add_argument(
+        '-m',
+        '--method',
+        choices=('chi2', 'entropy'),
+        default='entropy',
+        help='计算方言间相似度的方法'
+    )
+    parser.add_argument('-o', '--output', help='输出文件名')
+    parser.add_argument('input', default='.', help='输入方言字音文件所在目录')
+    args = parser.parse_args()
+
+    logging.getLogger().setLevel(logging.INFO)
+
+    output = f'{args.method}.csv' if args.output is None else args.output
+    logging.info(f'compute {args.method} similarity between dialects, ' \
+        f'input = {args.input}, output = {output}')
+
+    # 对每个字取第一个声母、韵母、声调均非空的读音
+    # 声韵调部分有值部分为空会导致统计数据细微偏差
+    data = sinetym.datasets.transform_data(
+        zhongguoyuyan.force_complete(
+            zhongguoyuyan.load_data(args.input)[[
+                'lid',
+                'cid',
+                'initial',
+                'final',
+                'tone'
+            ]]
+        ),
+        index='cid',
+        agg='first'
+    )
+
+    ids = data.columns.levels[0]
+
+    sim = getattr(similarity, args.method)(data.values, parallel=4)
+    pandas.DataFrame(sim, index=ids, columns=ids) \
+        .to_csv(output, line_terminator='\n')
