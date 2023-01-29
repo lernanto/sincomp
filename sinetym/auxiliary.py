d2ada90bd4322b80d85f50d8602092689df10795
黄艺华 <lernanto@foxmail.com>
2023-01-29 17:21:10 +0800

    optimize plotting code

--- sinetym/auxiliary.py
+++ sinetym/auxiliary.py
@@ -7,12 +7,17 @@
 __author__ = '黄艺华 <lernanto@foxmail.com>'
 
 
+import logging
 import pandas
 import numpy
 import scipy.sparse
-from sklearn.preprocessing import normalize, OrdinalEncoder
+from sklearn.preprocessing import normalize, OrdinalEncoder, StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.feature_extraction.text import CountVectorizer
+import colorspacious
+import matplotlib
+import shapely
+import cartopy
 
 
 def encode(data, dtype=numpy.int32, missing_values='', unknown_value=-1):
@@ -92,3 +97,90 @@ def vectorize(data, sep=' ', binary=False, dtype=numpy.int32, norm=None):
     numpy.cumsum(categories, out=limits[1:])
 
     return code, limits
+
+def pc2color(pc):
+    """
+    根据矩阵主成分分解的结果生成颜色，使样本点的颜色能反映主成分的差异.
+
+    取主成分的前两维变换至 Lab 色度的表示范围 [-50, 50]，亮度固定50，
+    然后从 Lab 色系转为 RGB 色系。
+
+    Parameters:
+        pc (`numpy.ndarray`): 矩阵分解的主成分，列数至少为2
+
+    Returns:
+        rgb (`numpy.ndarray`): RGB 颜色值，行数和 pc 相同
+    """
+
+    # 缩放传入的主成分使其接近高斯分布，标准差为0.4的高斯分布98%以上落在 [-1, 1] 区间
+    pc = StandardScaler().fit_transform(pc[:, :2]) * 0.4
+
+    # 把变换后的主成分拉伸到 Lab 色度的表示范围 [-50, 50]，亮度固定50
+    lab = numpy.empty((pc.shape[0], 3), dtype=numpy.float32)
+    lab[:, 0] = 50
+    lab[:, 1:] = pc * 50
+    if numpy.any(numpy.abs(lab[:, 1:]) > 50):
+        logging.warning(
+            f'{numpy.count_nonzero(numpy.any( numpy.abs(lab[:, 1:]) > 50, axis=1))}/'
+            f'{pc.shape[0]} points are out of [-50, 50], whose color may not show properly'
+        )
+
+    # Lab 色系转 RGB 色系用于显示
+    return numpy.clip(colorspacious.cspace_convert(lab, 'CIELab', 'sRGB1'), 0, 1)
+
+def make_clip_path(polygons, extent=None):
+    """
+    根据绘制范围及指定的多边形生成图形的裁剪路径.
+
+    Parameters:
+        polygons (`shapely.geometry.multipolygon.MultiPolygon`):
+            裁剪的范围，只保留该范围内的图形
+        extent: 绘制的范围 (左, 右, 下, 上)
+
+    Returns:
+        path (`matplotlib.path.Path`): 生成的裁剪路径，如果传入的多边形为空返回 None
+    """
+
+    if polygons is None:
+        return None
+
+    polygons = tuple(polygons) if hasattr(polygons, '__iter__') else (polygons,)
+    if len(polygons) == 0:
+        return None
+
+    if extent is not None:
+        # 先对绘图区域和裁剪区域取交集
+        xmin, xmax, ymin, ymax = extent
+        poly = shapely.geometry.Polygon((
+            (xmin, ymin),
+            (xmin, ymax),
+            (xmax, ymax),
+            (xmax, ymin),
+            (xmin, ymin)
+        ))
+        polygons = [poly.intersection(c) for c in polygons]
+
+    return matplotlib.path.Path.make_compound_path(
+        *cartopy.mpl.patch.geos_to_path(polygons)
+    )
+
+def clip_paths(paths, polygons, extent=None):
+    """
+    根据绘制范围及指定的多边形裁剪 matplotlib 绘制的图形.
+
+    Parameters:
+        paths (`matplotlib.PathCollection` or list of `matplotlib.PathCollection`):
+            待裁剪的图形
+        polygons (`shapely.geometry.multipolygon.MultiPolygon`):
+            裁剪的范围，只保留该范围内的图形
+        extent: 绘制的范围 (左, 右, 下, 上)
+    """
+
+    path = make_clip_path(polygons, extent=extent)
+    if path is not None:
+        # 裁剪图形
+        if hasattr(paths, '__iter__'):
+            for c in paths:
+                c.set_clip_path(path, transform=c.axes.transData)
+        else:
+            paths.set_clip_path(path, transform=paths.axes.transData)
