4274567396fd13f2ebd29b958f19e12b94f4f3b9
黄艺华 <lernanto@foxmail.com>
2023-05-13 17:08:09 +0800

    optimize code to compute rule compliance

--- sinetym/compare.py
+++ sinetym/compare.py
@@ -45,13 +45,13 @@ def load_rule(fname, characters=None):
 
     return rules
 
-def similarity(data, rules, norm='l2'):
+def compliance(data, rules, dtype=numpy.float32, norm='l2'):
     """
-    计算两组字集在各方言中的读音分布相似度.
-
+    计算方言字音对语音规则的符合度.
+    
     针对若干条读音规则，每条规则由2个字集组成，字集中每个字在一个方言中的读音为字集的读音分布，
-    2个字集的读音分布归一化后的内积为字集的读音相似度，取值为 [0, 1]。当取 L2 归一化时，
-    即为余弦相似度。
+    2个字集的读音分布归一化后的内积为字集的读音相似度，即方言对该规则的符合度，取值为 [0, 1]。
+    当取 L2 归一化时，即为余弦相似度。
 
     Parameters:
         data (`pandas.DataFrame`): 方言字音数据表
@@ -65,38 +65,36 @@ def similarity(data, rules, norm='l2'):
         similarities (`pandas.DataFrame`): 读音相似度数据表，每行为一个方言，每列为一条规则
     """
 
-    sim = []
-    for element in rules['element'].drop_duplicates():
+    comp = []
+    for element, rule in rules.groupby('element'):
         element_data = data.loc[:, pandas.IndexSlice[:, element]]
 
         # 先对方言读音 one-hot 编码
-        code, lim = auxiliary.vectorize(element_data)
+        code, lim = auxiliary.vectorize(element_data, dtype=dtype)
 
-        idx = rules.index[rules['element'] == element]
-        code1 = numpy.stack(
-            [code[data.index.get_indexer(c)].sum(axis=0).A.squeeze() \
-                for c in rules.loc[idx, 'cid1']],
-            axis=0
-        )
-        code2 = numpy.stack(
-            [code[data.index.get_indexer(c)].sum(axis=0).A.squeeze() \
-                for c in rules.loc[idx, 'cid2']],
-            axis=0
-        )
+        # 计算字集的读音向量
+        code1 = numpy.empty((rule.shape[0], code.shape[1]), dtype=dtype)
+        code2 = numpy.empty((rule.shape[0], code.shape[1]), dtype=dtype)
+        for i, (_, r) in enumerate(rule.iterrows()):
+            code1[i] = code[data.index.get_indexer(r['cid1'])].sum(axis=0).A[0]
+            code2[i] = code[data.index.get_indexer(r['cid2'])].sum(axis=0).A[0]
 
         # 计算读音分布相似度，对读音向量分别归一化后内积
-        sim.append(pandas.DataFrame(
-            numpy.stack(
-                [numpy.sum(
-                    normalize(code1[:, lim[i]:lim[i + 1]], norm=norm) \
-                        * normalize(code2[:, lim[i]:lim[i + 1]], norm=norm),
-                    axis=1
-                ) for i in range(lim.shape[0] - 1)],
-                axis=0
-            ),
+        sim = numpy.empty((element_data.shape[1], rule.shape[0]), dtype=dtype)
+        for i in range(element_data.shape[1]):
+            x1 = code1[:, lim[i]:lim[i + 1]]
+            x2 = code2[:, lim[i]:lim[i + 1]]
+            if norm is not None:
+                x1 = normalize(x1, norm=norm)
+                x2 = normalize(x2, norm=norm)
+
+            numpy.sum(x1 * x2, axis=1, out=sim[i])
+
+        comp.append(pandas.DataFrame(
+            sim,
             index=element_data.columns.get_level_values(0),
-            columns=idx
+            columns=rule.index
         ))
 
     # 结果数据按输入规则的顺序重新排序
-    return pandas.concat(sim, axis=1).reindex(rules.index, axis=1)
+    return pandas.concat(comp, axis=1).reindex(rules.index, axis=1)
