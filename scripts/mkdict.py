48c67eb9f135833ecca909153e104d6bc9d2b5f5
黄艺华 <lernanto@foxmail.com>
2023-09-01 19:55:39 +0800

    change dictionary format

--- scripts/mkdict.py
+++ scripts/mkdict.py
@@ -3,6 +3,9 @@
 
 """
 根据方言数据构建词典.
+
+读取环境变量 XIAOXUETANG_HOME 和 ZHONGGUOYUYAN_HOME 获取数据集的路径，
+然后读取数据集全部数据生成词典。
 """
 
 __author__ = '黄艺华 <lernanto@foxmail.com>'
@@ -13,44 +16,41 @@ import argparse
 import os
 import pandas as pd
 import csv
-import sinetym
+
+from sinetym.datasets import xiaoxuetang, zhongguoyuyan
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(globals().get('__doc__'))
-    parser.add_argument('-s', '--suffix', default='mb01dz.csv', help='方言数据文件后缀')
-    parser.add_argument('input_prefix', default='.', help='方言数据根目录')
+    parser.add_argument(
+        '-m',
+        '--min-freq',
+        type=int,
+        default=2,
+        help='出现频次不小于该值才计入词典'
+    )
     parser.add_argument('output_prefix', nargs='?', default='.', help='输出词典路径前缀')
     args = parser.parse_args()
 
     logging.getLogger().setLevel(logging.INFO)
-    logging.info(f'make dictionaries from {args.input_prefix} to {args.output_prefix}')
+    logging.info(f'make dictionaries to {args.output_prefix}')
 
-    dics = {}
-    for id in sorted(e.name[:-len(args.suffix)] \
-        for e in os.scandir(args.input_prefix) \
-        if e.is_file() and e.name.endswith(args.suffix)):
-        data = sinetym.datasets.load_data(
-            args.input_prefix,
-            id,
-            suffix=args.suffix
-        )
+    data = pd.concat([
+        xiaoxuetang,
+        zhongguoyuyan.assign(did=zhongguoyuyan['did'] + zhongguoyuyan['variant'])
+    ], axis=0, ignore_index=True)
 
-        for name in ('lid', 'cid', 'initial', 'final', 'tone'):
-            col = data.loc[data[name].notna() & (data[name] != ''), name]
-            if name in dics:
-                dics[name] = pd.concat([dics[name], col], ignore_index=True) \
-                    .drop_duplicates()
-            else:
-                dics[name] = col.drop_duplicates()
+    for name in ('did', 'cid', 'character', 'initial', 'final', 'tone'):
+        dic = data.loc[data[name] != '', name].value_counts().rename('count')
+        dic.index.rename(name, inplace=True)
 
-    for name, dic in dics.items():
+        if args.min_freq > 1:
+            dic = dic[dic >= args.min_freq]
+
         fname = f'{os.path.join(args.output_prefix, name)}.csv'
         logging.info(f'save dictionary {fname}')
-        dic.sort_values().to_csv(
+        dic.sort_values(ascending=False).to_csv(
             fname,
-            header=False,
-            index=False,
             encoding='utf-8',
             quoting=csv.QUOTE_NONE,
             lineterminator='\n'
