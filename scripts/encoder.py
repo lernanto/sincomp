8b88a38350f5c7c5acbf1cb1a3419414e567d550
黄艺华 <lernanto@foxmail.com>
2023-09-27 01:16:15 +0800

    encoder script uses new dictionaries and datasets

--- scripts/encoder.py
+++ scripts/encoder.py
@@ -12,22 +12,85 @@ import logging
 import argparse
 import os
 import json
-import pandas
-import numpy
-from sklearn.preprocessing import OrdinalEncoder
+import pandas as pd
+import numpy as np
 import tensorflow as tf
 from tensorboard.plugins import projector
 
-import sinetym
+import sinetym.models
+import sinetym.auxiliary
+from sinetym.datasets import xiaoxuetang, zhongguoyuyan
 
 
-def build_model(config, dialect_num, input_nums, output_nums):
+def load_dictionaries(prefix):
+    """
+    加载词典.
+
+    Parameters:
+        prefix (str): 词典路径前缀
+
+    Returns:
+        did, cid, intitial, final, tone (pd.Series): 词典列表
+    """
+
+    logging.info(f'load dictionaries from {prefix}')
+
+    dics = {}
+    for name in ('did', 'cid', 'character', 'initial', 'final', 'tone'):
+        dics[name] = pd.read_csv(
+            os.path.join(prefix, f'{name}.csv'),
+            index_col=0,
+            encoding='utf-8'
+        )
+
+    return (
+        dics['did'],
+        dics['cid'],
+        dics['character'],
+        dics['initial'],
+        dics['final'],
+        dics['tone']
+    )
+
+def load_datasets(config):
+    """
+    加载数据集.
+
+    Parameters:
+        config (dict): 数据集配置
+
+    Returns:
+        data (`pandas.DataFrame`): 所有数据集汇总成的数据表
+    """
+
+    logging.info(f'loading datasets...')
+
+    data = []
+    for d in config:
+        if isinstance(d, str):
+            dataset = getattr(sinetym.datasets, d)
+
+        else:
+            d = d.copy()
+            dataset = getattr(sinetym.datasets, d.pop('name')).filter(**d)
+
+        # 如果数据集包含方言变体，把变体编号加入为方言 ID 的一部分
+        if 'variant' in dataset.columns:
+            dataset = dataset.assign(did=dataset['did'] + dataset['variant'])
+
+        data.append(dataset[['did', 'cid', 'character', 'initial', 'final', 'tone']])
+
+    data = pd.concat(data, axis=0, ignore_index=True)
+
+    logging.info(f'done, {data.shape[0]} data loaded.')
+    return data
+
+def build_model(config):
     """
     根据配置创建模型和优化器.
 
     Parameters:
         config (dict): 多层级的配置字典，分析配置文件获得
-        dialect_num, input_nums, output_nums: 创建模型的参数
 
     Returns:
         model (sinetym.models.EncoderBase): 创建的编码器模型
@@ -37,7 +100,7 @@ def build_model(config, dialect_num, input_nums, output_nums):
     # 创建模型
     model_config = config.pop('model').copy()
     model_class = getattr(sinetym.models, model_config.pop('class'))
-    model = model_class(dialect_num, input_nums, output_nums, **model_config)
+    model = model_class(**model_config)
 
     # 创建优化器
     optimizer_config = config.pop('optimizer').copy()
@@ -68,11 +131,11 @@ def make_embeddings(
     为 TensorBoard embedding projector 显示向量创建需要的数据.
 
     Parameters:
-        location (`pandas.DataFrame`): 方言点信息数据表
-        char (`pandas.DataFrame`): 字信息数据表
-        initial (`pandas.Series`): 模型的声母表
-        final (`pandas.Series`): 模型的韵母表
-        tone (`pandas.Series`): 模型的声调表
+        location (`pd.DataFrame`): 方言点信息数据表
+        char (`pd.DataFrame`): 字信息数据表
+        initial (`pd.Series`): 模型的声母表
+        final (`pd.Series`): 模型的韵母表
+        tone (`pd.Series`): 模型的声调表
         encoder (`PredictorBase`): 方言字音预测模型
         output_path (str): 输出向量数据的路径前缀
     """
@@ -126,20 +189,12 @@ def make_embeddings(
 
     projector.visualize_embeddings(output_path, config)
 
-def benchmark(
-    config,
-    dialect_num,
-    input_nums,
-    output_nums,
-    train_data,
-    eval_data=None
-):
+def benchmark(config, train_data, eval_data=None):
     """
     评估各种配置的模型效果.
 
     Parameters:
         config (dict): 用于创建模型的配置字典
-        dialect_num, input_nums, output_nums: 创建模型的参数
         train_data (`tensorflow.data.Dataset`): 训练数据
         eval_data (`tensorflow.data.Dataset`): 评估数据
 
@@ -149,12 +204,7 @@ def benchmark(
     for conf in config['models']:
         conf = conf.copy()
         name = conf.pop('name')
-        model, optimizer = build_model(
-            conf,
-            dialect_num,
-            input_nums,
-            output_nums
-        )
+        model, optimizer = build_model(conf)
         output_path = os.path.join(config['output_dir'], name)
 
         logging.info(f'start training model {name}, output path = {output_path} ...')
@@ -167,22 +217,13 @@ def benchmark(
         )
         logging.info('done.')
 
-def train(
-    config,
-    name,
-    dialect_num,
-    input_nums,
-    output_nums,
-    train_data,
-    eval_data=None
-):
+def train(config, name, train_data, eval_data=None):
     """
     训练模型.
 
     Parameters:
         config (dict): 多层级的配置字典，分析配置文件获得
         name (str): 用于训练的模型配置名称，用于从 config 中读取指定配置
-        dialect_num, input_nums, output_nums: 创建模型的参数
         train_data (`tensorflow.data.Dataset`): 训练数据
         eval_data (`tensorflow.data.Dataset`): 评估数据
 
@@ -192,15 +233,10 @@ def train(
     # 从模型列表中查找待训练的模型
     conf = next((c for c in config['models'] if c['name'] == name)).copy()
     conf.pop('name')
-    model, optimizer = build_model(
-        conf,
-        dialect_num,
-        input_nums,
-        output_nums
-    )
+    model, optimizer = build_model(conf)
     output_path = os.path.join(config['output_dir'], name)
 
-    logging.info('start training ...')
+    logging.info(f'training model {name}, output path = {output_path}...')
     model.fit(
         optimizer,
         train_data,
@@ -210,32 +246,24 @@ def train(
     )
     logging.info('done.')
 
-def evaluate(
-    config,
-    name,
-    dialect_num,
-    input_nums,
-    output_nums,
-    eval_data
-):
+def evaluate(config, name, eval_data):
     """
     评估训练完成的模型效果.
 
     Parameters:
         config (dict): 多层级的配置字典，分析配置文件获得
         name (str): 用于训练的模型配置名称，用于从 config 中读取指定配置
-        dialect_num, input_nums, output_nums: 创建模型的参数
         eval_data (`tensorflow.data.Dataset`): 评估数据
     """
 
     conf = next((c for c in config['models'] if c['name'] == name)).copy()
-    model, _ = build_model(conf, dialect_num, input_nums, output_nums)
+    model, _ = build_model(conf)
 
     checkpoint = tf.train.Checkpoint(model=model)
     manager = tf.train.CheckpointManager(
         checkpoint,
         os.path.join(config['output_dir'], name, 'checkpoints'),
-        max_to_keep=20
+        None
     )
     logging.info(f'restore model from checkpoint {manager.latest_checkpoint} ...')
     checkpoint.restore(manager.latest_checkpoint)
@@ -259,73 +287,42 @@ if __name__ == '__main__':
     logging.info(f'load configuration from {args.config}')
     config = json.load(args.config)
 
-    # 加载词典
-    logging.info(f'load dictionaries from {config["dictionary_dir"]}')
-    for name in ('lid', 'cid', 'initial', 'final', 'tone'):
-        locals()[name] = pandas.Series(open(
-            os.path.join(config['dictionary_dir'], f'{name}.csv'),
-            encoding='utf-8'
-        )).str.rstrip('\r\n')
+    did, cid, character, initial, final, tone = load_dictionaries(config['dictionary_dir'])
+    did = did[:config['dialect_num'] - 1]
+    cid = cid[:config['input_nums'][0] - 1]
+    character = character[:config['input_nums'][1] - 1]
+    initial = initial[:config['output_nums'][0] - 1]
+    final = final[:config['output_nums'][1] - 1]
+    tone = tone[:config['output_nums'][2] - 1]
 
-    cid = cid.astype(int)
-
-    logging.info(f'load data from {config["data_dir"]}')
-    dialect_path = os.path.join(config['data_dir'], 'dialect')
-    location = sinetym.datasets.zhongguoyuyan.load_location(
-        os.path.join(config['data_dir'], 'location.csv')
-    ).reindex(lid)
-    char = pandas.read_csv(
-        os.path.join(config['data_dir'], 'words.csv'),
-        index_col=0
-    ).reindex(cid)
- 
-    data = sinetym.datasets.load_data(dialect_path, suffix='mb01dz.csv')
-    data = OrdinalEncoder(
-        categories=[lid, cid, initial, final, tone],
-        dtype=numpy.int32,
-        handle_unknown='use_encoded_value',
-        unknown_value=-1
-    ).fit_transform(data[['lid', 'cid', 'initial', 'final', 'tone']])
+    data = load_datasets(config['datasets'])
+    data = sinetym.auxiliary.OrdinalEncoder(
+        categories=[
+            did.index,
+            cid.index,
+            character.index,
+            initial.index,
+            final.index,
+            tone.index
+        ],
+        dtype=np.int32
+    ).fit(data[:1]).transform(data)
 
     data = tf.data.Dataset.from_tensor_slices((
         data[:, 0],
-        data[:, 1:2],
-        data[:, 2:5]
+        data[:, 1:3],
+        data[:, 3:6]
     ))
     data = data.shuffle(data.cardinality(), seed=10273)
 
     if args.command == 'train':
-        logging.info(f'train {args.model}, input = {config["data_dir"]}')
-        train(
-            config,
-            args.model,
-            location.shape[0],
-            (char.shape[0],),
-            (initial.shape[0], final.shape[0], tone.shape[0]),
-            data
-        )
+        train(config, args.model, data)
 
     elif args.command == 'evaluate':
-        logging.info(f'evaluate {args.model}, input = {config["data_dir"]}')
-        evaluate(
-            config,
-            args.model,
-            location.shape[0],
-            (char.shape[0],),
-            (initial.shape[0], final.shape[0], tone.shape[0]),
-            data
-        )
+        evaluate(config, args.model, data)
 
     elif args.command == 'benchmark':
-        logging.info(f'benchmark, input = {config["data_dir"]}')
         eval_size = data.cardinality() // 10
         train_data = data.skip(eval_size)
         eval_data = data.take(eval_size)
-        benchmark(
-            config,
-            location.shape[0],
-            (char.shape[0],),
-            (initial.shape[0], final.shape[0], tone.shape[0]),
-            train_data,
-            eval_data
-        )
+        benchmark(config, train_data, eval_data)
