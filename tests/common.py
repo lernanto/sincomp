bfa3fda931705538181862b482af94444d6fdeef
黄艺华 <lernanto@foxmail.com>
2024-12-29 23:11:15 +0800

    test: support saving files to temporary dictionary during unit tests

--- tests/common.py
+++ tests/common.py
@@ -16,7 +16,7 @@ import urllib.request
 
 
 data_dir = os.path.join(os.path.dirname(__file__), 'data')
-cache_dir = tempfile.TemporaryDirectory().name
+tmp_dir = tempfile.TemporaryDirectory().name
 
 
 def mock_urlopen(url, *args, **kwargs):
@@ -33,13 +33,13 @@ def mock_urlopen(url, *args, **kwargs):
 
 
 def setUpModule():
-    """为缓存文件创建临时目录，为数据集设置环境变量，使用模拟函数取代真正的网络请求"""
+    """为测试产生的文件创建临时目录，为数据集设置环境变量，使用模拟函数取代真正的网络请求"""
 
     global env_patcher
     global urlopen_patcher
 
     env_patcher = unittest.mock.patch.dict(os.environ, {
-        'SINCOMP_CACHE': cache_dir,
+        'SINCOMP_CACHE': os.path.join(tmp_dir, 'cache'),
         'ZHONGGUOYUYAN_HOME': os.path.join(data_dir, 'zhongguoyuyan')
     })
     urlopen_patcher = unittest.mock.patch.object(
